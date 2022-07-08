2e806ec72819b2b388531c2d882023bfaa539e48
toyrik <toyrik@gmail.com>
2022-07-08 09:10:44 +0300

    добавляем тестовый роут для добавления книги

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,15 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
 use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    public function __construct(private BookRepository $bookRepository)
+    public function __construct(private BookRepository $bookRepository, private EntityManagerInterface $em)
     {
+    }
+
+    #[Route('/newBook')]
+    public function addNewBook(): Response
+    {
+        $book = new Book();
+        $book->setTitle('Tutorial');
+        $this->em->persist($book);
+        $this->em->flush();
+
+        return new Response();
     }
 
     #[Route('/')]
