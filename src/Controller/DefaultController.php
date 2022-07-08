0d4e899763c4b535aa25fb319dac9025707c35cb
toyrik <toyrik@gmail.com>
2022-07-08 08:59:51 +0300

    добавляем сущность книги

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,15 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
+    public function __construct(private BookRepository $bookRepository)
+    {
+    }
+
     #[Route('/')]
     public function root(): Response
     {
-        return $this->json(['test' => 'hello']);
+        $books = $this->bookRepository->findAll();
+
+        return $this->json($books);
     }
 }
